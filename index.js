c85fdc22d0801f1fb69a394b9672d68d0d979326
Acidiney Dias <acidineydias@gmail.com>
2020-05-12 20:08:51 +0100

    test heroku

--- index.js
+++ index.js
@@ -2,61 +2,63 @@
  * @author: Acidiney Dias <acidineydias@gmail.com>
  * @description: Get A three last posts from a public instagram account
  * @date 07/02/2020
-*/
+ */
 
-'use strict'
-const axios = require('axios')
+const axios = require('axios');
+const app = require('express')();
 
-const publicCredentials = {
-	username: 'acidineydias',
-}
-
-let profileData = {}
+let profileData = {};
 
 // get profile data from instagram
-async function _getProfileDataFromInstagram () {
-	try {
-		const { data } = await axios.get(`https://www.instagram.com/${publicCredentials.username}/?__a=1`)
-		profileData = data
-	} catch (e) {
-		throw "Cannot comunicate with instagram \ " + e
-	}
+async function _getProfileDataFromInstagram(id) {
+  try {
+    const { data } = await axios.get(`https://www.instagram.com/${id}/?__a=1`);
+    profileData = data;
+  } catch (e) {
+    return e.message
+  }
 }
 
-function getTitleFromPosts (instaPost) {
-	const { edge_media_to_caption: post } = instaPost
-	// get the last edition from edge text node
-	return post.edges[post.edges.length - 1] && post.edges[post.edges.length - 1].node.text
+function getTitleFromPosts(instaPost) {
+  const { edge_media_to_caption: post } = instaPost;
+  // get the last edition from edge text node
+  return (
+    post.edges[post.edges.length - 1] &&
+    post.edges[post.edges.length - 1].node.text
+  );
 }
 
-;(async function () {
-	await _getProfileDataFromInstagram()
-	
-	//retrieve user from insta graphql
-	const { user = {} } = profileData.graphql
-	// console.log(user)
+app.get('/:id', async function (req, res) {
+  const error = await _getProfileDataFromInstagram(req.params.id);
+	if (error) res.end(error)
+  //retrieve user from insta graphql
+  const { user = {} } = profileData.graphql;
+  // console.log(user)
 
-	// retrieve timeline from user
+  // retrieve timeline from user
 
-	const { edge_owner_to_timeline_media: timeline } = user
+  const { edge_owner_to_timeline_media: timeline } = user;
 
-	// retrieve posts from user
-	const { edges: posts } = timeline
+  // retrieve posts from user
+  const { edges: posts } = timeline;
 
-	// get the last three post
-	const recentPosts = posts.slice(0, 3)
+  // get the last three post
+  const recentPosts = posts.slice(0, 3);
 
-	recentPosts.forEach(post => {
-		const { node } = post
+  const b = recentPosts.map((post) => {
+    const { node } = post;
 
-		// print on console text from posts
-		console.table({
-			text: getTitleFromPosts(node),
-			thumb: node.display_url,
-			isVideo: node.is_video,
-			likes: node.edge_liked_by.count,
-			comments: node.edge_media_to_comment.count
-		})
-	})
+    // print on console text from posts
+    return {
+      text: getTitleFromPosts(node),
+      thumb: node.display_url,
+      isVideo: node.is_video,
+      likes: node.edge_liked_by.count,
+      comments: node.edge_media_to_comment.count,
+    };
+  });
 
-})()
+  res.end(JSON.stringify(b));
+});
+
+app.listen(3000);
